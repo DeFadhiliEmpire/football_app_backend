60e92fc2fabc4b06c31d983d4c4e398ea00206c7
DeFadhiliEmpire <catarax27@gmail.com>
2026-05-25 22:47:03 +0300

    Change SMTP port to 465 for secure connection

--- auth.js
+++ auth.js
@@ -58,7 +58,7 @@ const User = mongoose.model("User", userSchema);
 //Email helper
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT) || 587,
+  port: Number(process.env.SMTP_PORT) || 465,
   secure: false,
   auth: { user: process.env.SMTP_USER, pass: process.env.SMTP_PASS },
 });
