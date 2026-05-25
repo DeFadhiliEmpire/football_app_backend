e52642877ba145a7483ba4371ccb5d3e877564e1
DeFadhiliEmpire <catarax27@gmail.com>
2026-05-25 23:19:33 +0300

    Update SMTP configuration to use port 587

--- auth.js
+++ auth.js
@@ -58,8 +58,9 @@ const User = mongoose.model("User", userSchema);
 //Email helper
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT) || 465,
-  secure: true,
+  port: Number(process.env.SMTP_PORT) || 587,
+  secure: false,
+  family:4,
   auth: { user: process.env.SMTP_USER, pass: process.env.SMTP_PASS },
 
   connectionTimeout: 30000,
